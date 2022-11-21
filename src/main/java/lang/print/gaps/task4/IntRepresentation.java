de9459d9dfab012bb76a81b0363d53021f4e7014
Stas Sukora <st.sukora@gmail.com>
2022-11-22 00:44:47 +0300

    Update readme.md

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,16 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+//        int first = c;
+//        int second = h;
+//        int third = a;
+//        int four = r;
+
+        System.out.println((int) c + " " + (int) h + " " + (int) a + " " + (int) r);
     }
 }
